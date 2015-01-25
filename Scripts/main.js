f1e62ef72d67ed10900cb17f0936216201a9a45d
crotten <wildrunts@gmail.com>
2015-01-25 18:46:39 -0500

    working on the team pages and styling

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,10 @@
+$(document).ready(function() {
 
+  $(".group").on("mouseover", function() {
+    $(this).css("opacity", "0.5");
+  });
+  $(".group").on("mouseout", function() {
+    $(this).css("opacity", "1");
+  });
+
+});
